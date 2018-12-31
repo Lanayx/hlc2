0d9346dbb348cc08177c649432260caa02780e7b
Vladimir Shchur <shchurvladimir@gmail.com>
2018-12-31 12:45:08 +0300

    Changed dictionary to array

--- src/Benchmarks/Array.fs
+++ src/Benchmarks/Array.fs
@@ -28,21 +28,32 @@ type ArrayBenchmarks() =
         |> Seq.toArray
 
     [<Benchmark>]
-    member this.Simple() =
+    member this.SeqCompExpr() =
         let arr = this.arr
         seq {
             let mutable i = 1
-            while i > 0 do
+            while i <= arr.Length do
                 yield arr.[arr.Length - i]
-                i <- i + 1                
+                i <- i + 1
         }
         |> Seq.map (fun x -> x * x)
         |> Seq.filter (fun x -> x % 2 = 0)
         |> Seq.take 5
         |> Seq.toArray
 
+    [<Benchmark>]
+    member this.SeqLambda() =
+        let arr = this.arr
+        Seq.init arr.Length (fun i -> arr.[arr.Length - i - 1])
+        |> Seq.map (fun x -> x * x)
+        |> Seq.filter (fun x -> x % 2 = 0)
+        |> Seq.take 5
+        |> Seq.toArray
+
+
 //       Method |       Mean |     Error |    StdDev | Gen 0/1k Op | Gen 1/1k Op | Gen 2/1k Op | Allocated Memory/Op |
 //------------- |-----------:|----------:|----------:|------------:|------------:|------------:|--------------------:|
-// ReverseArray | 8,369.5 ns | 166.03 ns | 272.79 ns |      3.2654 |           - |           - |             10296 B |
-//   ReverseSeq | 2,349.2 ns | 136.48 ns | 402.41 ns |      1.5411 |           - |           - |              4856 B |
-//       Simple |   772.7 ns |  15.40 ns |  37.48 ns |      0.2737 |           - |           - |               864 B |
+// ReverseArray | 5,507.5 ns | 134.72 ns | 132.31 ns |      2.4490 |           - |           - |             10296 B |
+//   ReverseSeq | 1,939.0 ns |  35.71 ns |  31.66 ns |      1.1559 |           - |           - |              4856 B |
+//  SeqCompExpr |   809.8 ns |  16.05 ns |  25.91 ns |      0.2050 |           - |           - |               864 B |
+//    SeqLambda | 1,868.3 ns |  39.09 ns |  67.43 ns |      0.6294 |           - |           - |              2656 B |
